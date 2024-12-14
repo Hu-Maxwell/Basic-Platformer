9af1be5a465237b9dea17e0f3d15b4e616adec4b
dewe biag <dewebiag@gmail.com>
2024-12-14 14:14:10 -0800

    updated birdDash styling

--- Assets/Character/Movement/BirdDash.cs
+++ Assets/Character/Movement/BirdDash.cs
@@ -4,8 +4,7 @@ using System.Collections; // allows for IEnumerator
 /// <summary>
 /// executes the bird's dash movement by applying horizontal velocity and disabling gravity
 /// </summary>
-public class BirdDash : BirdCore
-{
+public class BirdDash : BirdCore {
     #region vars
     [HideInInspector] public bool isDashing;
     [HideInInspector] public float timeSinceDashEnd;
@@ -18,51 +17,41 @@ public class BirdDash : BirdCore
     
     #endregion
 
-    public void Start() 
-    {
+    public void Start() {
         originalGravityScale = rb.gravityScale;
     }
 
-    public void Update()
-    {
+    public void Update() {
         timeSinceDashEnd += Time.deltaTime;
     }
 
-    private void ApplyDashForce(float direction)
-    {
+    private void ApplyDashForce(float direction) {
         rb.gravityScale = 0;
         rb.linearVelocity = new Vector2(birdWalk.MoveSpeed * dashMultiplier * direction, 0);
     }
 
-    private void RestorePostDash(float oldVelX)
-    {
+    private void RestorePostDash(float oldVelX) {
         rb.gravityScale = originalGravityScale;
 
         // prevents bird from looking in opposite dir after dash ends
         if (Mathf.Sign(oldVelX) != birdDirection.lookingDirectionX)
-        {
             rb.linearVelocityX = 0; 
-        }
         else
-        {
             rb.linearVelocityX = oldVelX;
-        }
 
         timeSinceDashEnd = 0;
         birdWalk.disableWalk = false;
         isDashing = false;
     }
 
-    private float GetDashDirection()
-    {
+    private float GetDashDirection() {
         // reverses direction if bird is touching wall
         return birdJump.isTouchingWall 
             ? -birdDirection.lookingDirectionX 
             : birdDirection.lookingDirectionX;
     }
 
-    public IEnumerator Dash()
-    {
+    public IEnumerator Dash() {
         birdWalk.disableWalk = true;
         isDashing = true;
         firstDash = false; 
