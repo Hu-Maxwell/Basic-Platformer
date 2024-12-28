fb751723e3350b07763a5ce6f3672ab11f4d69db
dewe biag <dewebiag@gmail.com>
2024-12-27 23:57:03 -0800

    clamp above y = -1

--- Assets/Camera/CameraFollower.cs
+++ Assets/Camera/CameraFollower.cs
@@ -5,7 +5,7 @@ public class CameraFollower : CameraCore
     /// <summary>
     /// makes camera follow the character around
     /// </summary>
-
+    /// 
 
     // take bird's transform (for camera follower)
     public Transform targetTransform; 
@@ -20,15 +20,15 @@ public class CameraFollower : CameraCore
         if (targetTransform == null) {
             return;
         }
-
-        Vector3 currentPosition = transform.position; 
+        Vector3 currentPosition = transform.position;
 
         Vector3 targetPosition = new Vector3(
             targetTransform.position.x + offset.x,
-            targetTransform.position.y + offset.y,
-            currentPosition.z 
+            Mathf.Max(targetTransform.position.y + offset.y, -1f), 
+            currentPosition.z
         );
 
+        // Smoothly interpolate between the current position and the target position
         transform.position = Vector3.Lerp(currentPosition, targetPosition, smoothSpeed);
     }
 }
