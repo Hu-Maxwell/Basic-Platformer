72cba5a815e6ea972fcc6e687b4b6b0d617d41a9
dewe biag <dewebiag@gmail.com>
2025-01-09 23:59:19 -0800

    camera input

--- Assets/Camera/CameraInput.cs
+++ Assets/Camera/CameraInput.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 public class CameraInput : CameraCore {
@@ -5,11 +6,31 @@ public class CameraInput : CameraCore {
     /// <summary>
     /// moves the camera around based off of user input
     /// </summary>
+    
+    public float lookKeyHeldTimer; 
+
+
     void Start() {
         
     }
 
     void Update() {
-        
+        CheckInputs();
+    }
+
+    public void CheckInputs() { 
+        if (Input.GetKey(KeyCode.W)) {
+            lookKeyHeldTimer+= Time.deltaTime;
+            if (lookKeyHeldTimer >= 2) {
+                ShiftCameraY(); 
+            }
+        } else {
+            lookKeyHeldTimer = 0; 
+        }
+    }
+
+    // takes as a parameter whether to look up or down, then looks wherever
+    void float ShiftCameraY() {
+        return 
     }
 }
